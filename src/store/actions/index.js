fb398acfe4c03f3b646acbd0397b4f01a8dcd15e
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-08-14 14:38:23 -0500

    WIP: built out actions and logic in App.js

---
+++ src/store/actions/index.js
@@ -0,0 +1,11 @@
+export const REMOVE_FEATURE = 'REMOVE_FEATURE';
+
+export const BUY_ITEM = 'BUY_ITEM';
+
+export const remove = item => {
+    return {type: REMOVE_FEATURE, payload: item};
+};
+
+export const buy = item => {
+    return { type: BUY_ITEM, payload: item};
+};
